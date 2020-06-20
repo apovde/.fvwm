50ba48ec32fdcafa0580efb23226eb000e3a8e9f
Anton Popov <mail@antonpopov.de>
2020-06-20 15:35:20 +0200

    Fix: DisableTouchpad: Add anchor to regex

--- bin/disable-touchpad.sh
+++ bin/disable-touchpad.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-id=$(xinput list | grep -i touchpad | sed -e 's/.*id=//;s/\t.*$//')
+id=$(xinput list | grep -i touchpad | sed -e 's/^.*id=//;s/\t.*$//')
 
 xinput set-prop $id "Device Enabled" 0
